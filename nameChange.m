77b86dffdc1fe712b8aa0cfaf1f638ad8abe3175
TristanShoemaker <trstn.shmkr@gmail.com>
2016-07-08 12:26:57 +0200

    Delete nameChange.m

--- nameChange.m
+++
@@ -1,7 +0,0 @@
-files = dir('/data/procdata/detchar/env/Schumann/summer2016/VCdata1/PSD*.mat');
-for i = 1:length(files)
-    origName = files(i).name;
-    newName = strrep(origName,'PSD_23-27','')
-    movefile(origName,newName)
-end
-        
